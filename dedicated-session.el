fee3945cb235c2221a975faa73e85a8d98bb1f68
herman <hermanhelf@yeah.net>
2022-05-04 18:01:17 +0800

    v1 the first working version
    contains all basic functionalities, the process is complete, with
    dumping, doing, toggle between rest and bar, and releasing.
    the automatic toggle between rest and bar with set timer is not yet
    implemented in this version

--- dedicated-session.el
+++ dedicated-session.el
@@ -30,17 +30,37 @@
 					      :duration "duration")
   "the properties used by journal entries. Used to keep track of the properties(and avoid typo)")
 
+(defvar dedicated-session-buffer-plist nil
+  "plist storing properties used by the buffer actions. switch-buffer is used mostly to pop buffers, in which kill-current-buffer would automatically restore the last use buffer, so no need to store window configurations like org-capture in this stage. Therefore it's temprory not in use."
+  )
+
 (defvar dedicated-session-default-dumping-prompt-fmt
-  "This stage you dump everything irrelevent to %s out of your brain.\n"
+  "This stage you dump everything irrelevent to =%s= out of your brain.\n"
   "the default dumping prompt format used in the dumping buffer")
+
+(defvar dedicated-session-default-resting-prompt-fmt
+  "rest in the doing session to keep your focus on =%s= sharp.\n"
+  "default rest prompt format used in the rest in the doing")
+
+(defvar dedicated-session-default-releasing-prompt-fmt
+  "now give away your attention on =%s=, as its spotlight fading"
+  "default releasing prompt format used in releasing buffer")
 
 (defvar dedicated-session-bar/rest-number 0
   "mark the nth bar/rest. make the heading in journal unique."
   )
 
+(defvar dedicated-session-log-number-this-session 0
+  "how many logs have been taken. used to generate log title. could make (last-log) function to navigate between them using re-expr.")
+
+(defvar dedicated-session-log-capture-template-string nil
+  "the string, like \"tt\", associated with your org-capture template for the log template
+an example of it could be")
+
+
 
 ;; test environment
-(setq dedicated-session-journal "~/playground/projects/dedicated-session-journal.org")
+(setq dedicated-session-journal "~/playground/projects/dedicated-session/dedicated-session-journal.org")
 (setq dedicated-session-source "./dedicated-session-source.org")
 ;; end of test environment
 
@@ -141,8 +161,8 @@ usage:
  (org-mode) (dedicated-session-find-current-entry-in-journal))"
   (let* ((entry-type dedicated-session-in)
 	 (headline-string (cond ((equal 'dumping entry-type) '("dumping"))
-			       ((equal 'doing/bar entry-type) '("doing" (format  "bar %d" (floor (/ dedicated-session-bar/rest-number 2)))))
-			       ((equal 'doing/rest entry-type) '("doing" (format "rest %d" (floor (/ dedicated-session-bar/rest-number 2)))))
+			       ((equal 'doing/bar entry-type) `("doing" ,(format  "bar %d" (ceiling (/ (+ 1 dedicated-session-bar/rest-number ) 2)))))
+			       ((equal 'doing/rest entry-type) `("doing" ,(format "rest %d" (ceiling (/ (+ 1 dedicated-session-bar/rest-number ) 2)))))
 			       ((equal 'releasing entry-type) '("releasing"))
 			       ))
 	 (olp (list (format-time-string "%Y" dedicated-session-current-start-time)
@@ -171,32 +191,46 @@ usage:
       (dedicated-session-find-current-entry-in-journal)
       
       (cond
-       ((-contains? '(:start-time :end-time) property)
+       ((member property '(:start-time :end-time) )
 	(org-set-property property-string (format-time-string "<%Y-%m-%d %H:%M>" (org-current-time))))
        ((equal :duration property)
 	(org-set-property property-string
-			  (format-time-string
-			   "%H:%M"
+			  (format-seconds
+			   "%02h:%02m"
 			   (time-subtract 
-			    (org-time-string-to-time (org-ml-get-property "start-time" (org-ml-parse-this-headline)))
-			    (org-time-string-to-time (org-ml-get-property "end-time" (org-ml-parse-this-headline))))))
-	)    
-       (write-region (point-min) (point-max) dedicated-session-journal)
-       ))))
+			    (org-time-string-to-time (org-element-property :END-TIME (org-element-at-point)))
+			    (org-time-string-to-time (org-element-property :START-TIME (org-element-at-point)))
+			    )))
+	))    
+      (write-region (point-min) (point-max) dedicated-session-journal)
+       )))
 
 
 
 ;;minor mode dedicated-session-dumping
-(define-minor-mode dedicated-session-dumping-mode
+(define-minor-mode dedicated-session-resting-mode
   "a minor mode for the dumping buffer. Mainly just to set C-c C-c keybinding."
-  :lighter " dumping"
+  :lighter " resting"
   :keymap (let ((map (make-sparse-keymap)))
-	    (define-key map (kbd "C-c C-c") 'dedicated-session-dumping-finalize)
+	    (define-key map (kbd "C-c C-c") 'dedicated-session-resting-finalize)
 	    map))
-(defun dedicated-session-dumping-finalize ()
-  "close the dumping buffer and enter doing stage"
+(defun dedicated-session-resting-finalize ()
+  "close the rest promt buffer and: enter doing stage if now in dumping;enter next bar if now in doing/rest, end session if now in releasing"
   (interactive)
   (kill-buffer (current-buffer))
+  (cond
+   ((equal dedicated-session-in 'dumping)
+    (dedicated-session-journal-set-property :end-time)
+    (dedicated-session-journal-set-property :duration)
+    (dedicated-session-doing))
+   ((equal dedicated-session-in 'doing/rest)
+    (dedicated-session-bar-rest-toggle))
+   ((equal dedicated-session-in 'releasing)
+    (progn (dedicated-session-journal-set-property :end-time)
+	   (dedicated-session-journal-set-property :duration))
+    (dedicated-session-end))
+   )
+  
   )
 
 (defun dedicated-session-read-source (thing)
@@ -205,6 +239,7 @@ usage:
   
   (let ((current-tag (cond ((equal dedicated-session-in 'dumping) "dumping")
 			   ((equal dedicated-session-in 'doing/rest) "doing")
+			   ((equal dedicated-session-in 'doing/bar) "doing")
 			   ((equal dedicated-session-in 'releasing) "releasing")
 			   )
 		     )
@@ -212,23 +247,28 @@ usage:
    (with-temp-buffer
      (insert-file-contents dedicated-session-source)
      (org-mode)
+     (setq end-of-subtree? '(t t)) ;;2 time to pop
      (setq buffer (org-ml-parse-this-buffer))
      (setq tops (org-ml-headline-get-subheadlines buffer))
      (hermanhel/org-find-headline-in-buffer-olp '("rest methods"))     
      (if (outline-has-subheading-p)
 	 (progn (org-next-visible-heading 1)
-		(while (not (equal (save-excursion (org-forward-heading-same-level 1) (point)) (point)))
-		  
-		  ;;(message (nth 4 (org-heading-components)))
+		(while  end-of-subtree?
+		
+		  (message (nth 4 (org-heading-components)))
 		  (message current-tag)
 		  (when (member current-tag (org-get-tags))
 		     (add-to-list 'read-result (nth 4 (org-heading-components)))
-		      (add-to-list 'read-result (org-get-entry)))
-		  (org-forward-heading-same-level 1)))
+		      (add-to-list 'read-result  (org-get-entry)))
+		  (org-forward-heading-same-level 1)
+		  (when (equal (save-excursion (org-forward-heading-same-level 1) (point)) (point))
+		    ;; pop 1 item out of end-of-subtree? everytime meet end. The second time would make it nil
+		    (setq end-of-subtree? (cdr end-of-subtree?)))
+		  ))
        (user-error "rest methods have no methods to show. Add at least one in %s" dedicated-session-source))
      read-result
      )))
-(member "dumping" '("dumping" "releasing"))
+
 
 
 
@@ -253,11 +293,38 @@ usage:
   (setq dedicated-session-current-start-time (org-current-time))
   ;;; open journal file
   (dedicated-session-journal-set-property :start-time)
+  (dedicated-session-rest-prompt dedicated-session-default-dumping-prompt-fmt)
+  )
+
+(defun dedicated-session-in-string ()
+  "give string based on the stage now is in"
+  (cond
+   ((equal dedicated-session-in 'dumping) "dumping")
+   ((equal dedicated-session-in 'doing/rest) "rest")
+   ((equal dedicated-session-in 'doing/bar) "bar")
+   ((equal dedicated-session-in 'releasing) "releasing")
+   ))
+
+
+
+
+
+(defun dedicated-session-rest-prompt (prompt-fmt)
+  "display a prompt buffer start with:
+* head
+prompt(with a optional slot for topic)
+and display matched rest methods with:
+** rest method 1 matched
+descriptions
+
+"
   (switch-to-buffer "*dedicated-session*")
   (org-mode)
-  (dedicated-session-dumping-mode)
-  (insert "* Dumping\n")
-  (insert (format dedicated-session-default-dumping-prompt-fmt dedicated-session-topic))
+  (dedicated-session-resting-mode)
+  (setq head (dedicated-session-in-string))
+  (insert (format "* %s" head))
+  (org-return)
+  (insert (format prompt-fmt dedicated-session-topic))
   (org-return)
   (setq rest-methods (dedicated-session-read-source 'rest-methods))
   (while rest-methods
@@ -270,18 +337,19 @@ usage:
     (setq rest-methods (butlast rest-methods)))
   )
 
-
 ;;doing functions
 ;;doing functions are called 
 
 (defun dedicated-session-doing ()
   "start doing stage"
   (setq dedicated-session-in 'doing)
+  (message "Starting Doing!!!")
   ;; start a bar
   (dedicated-session-bar-rest-toggle)
   )
 (defun dedicated-session-bar-rest-toggle (&optional done?)
   "toggle between bar and rest in doing stage"
+  (interactive)
   (unless (member dedicated-session-in '(doing doing/bar doing/rest))
     (user-error "you must toggle between bar and rest in doing stage"))
   ;;log end-time
@@ -290,21 +358,111 @@ usage:
     (progn (dedicated-session-journal-set-property :end-time)
 	   (dedicated-session-journal-set-property :duration)))
   ;;toggle state
-  (setq dedicated-session-in (cond ((member dedicated-session-in '(doing doing/rest))
-				    'doing/bar)
-				   ((equal dedicated-session-in 'doing/bar)
-				    'doing/rest)))
+  (setq dedicated-session-in (cond
+			      (done? 'releasing)
+			      ((member dedicated-session-in '(doing doing/rest))
+			       'doing/bar)
+			      ((equal dedicated-session-in 'doing/bar)
+			       'doing/rest)
+			      ))
   (if (not done?)
       (progn
        ;;inc the bar number
-       (setq dedicated-session-bar/rest-number (incf dedicated-session-bar/rest-number))
+	(cl-incf dedicated-session-bar/rest-number)
        ;;log start-time. if done? then not log start-time
        (dedicated-session-journal-set-property :start-time)
+       (message (concat (dedicated-session-in-string) " " (number-to-string (ceiling (/ (+ 1 dedicated-session-bar/rest-number) 2))) " on " dedicated-session-topic " has started!!!"))
        (when (equal dedicated-session-in 'doing/rest)
 	 (dedicated-session-doing-rest-prompt)))
-    nil)
+    (dedicated-session-journal-set-property :start-time)
+    (message "releasing " dedicated-session-topic))  
+  )
+
+
+(defun dedicated-session-doing-rest-prompt ()
+  "display rest prompt similar to dumping"
+  (dedicated-session-rest-prompt dedicated-session-default-resting-prompt-fmt)
+  (message "resting resting!")
+  )
+
+(defun dedicated-session-end ()
+  "end the session"
+  (setq dedicated-session-in nil)
+  (setq dedicated-session-current-start-time nil)
+  (setq dedicated-session-topic nil)
+  (setq dedicated-session-bar/rest-number 0)
+  (setq dedicated-session-log-number-this-session 0))
+
+;;log function
+(define-minor-mode dedicated-session-log-mode
+  "minor for dedicated-session-log buffer. mainly just to set C-c C-c keybinding"
+  :lighter " log"
+  :keymap (let ((map (make-sparse-keymap)))
+	    (define-key map (kbd "C-c C-c") 'dedicated-session-log-finalize)
+	    map))
+
+(defun dedicated-session-log (&optional summary?)
+  "a simple log function popping an org buffer with minimum template.
+if call with summary? t, means it's called by dedicated-session-releasing-prompt"
+  (interactive)
+  (condition-case nil
+      (org-store-link '() t) ;; t then will be inserted automatically
+    (error nil))
+  (switch-to-buffer "*dedicated-session-log*")
+  (org-mode)
+  (dedicated-session-log-mode)
+  (if (member dedicated-session-in '(dumping releasing))
+      (insert "****** ")
+    (insert "******* ")
+    )
+  (if summary?
+      (insert "summary")
+      (insert (format "log #%d" (+ 1 dedicated-session-log-number-this-session))))
+  (org-set-property "log-time" (format-time-string "[%Y-%m-%d %R]" (org-current-time)))
+  (goto-char (point-max))
+  (org-insert-link)
+  (org-return)
+  )
+
+(setq dedicated-session-in 'doing/rest)
+
+(defun dedicated-session-log-finalize ()
+  "store the entry in dedicated-session-journal, and kill the buffer"
+  (interactive)
+  (goto-char (point-min))
+  (if (equal (nth 4 (org-heading-components)) "summary")
+      (setq summary? t)
+    (setq summary? nil))
   
-  
+  (setq text (buffer-string))
+  (with-temp-buffer
+    (insert-file-contents dedicated-session-journal)
+    (org-mode) (dedicated-session-find-current-entry-in-journal)
+    (goto-char (org-end-of-subtree))
+    (org-return)
+    (insert text)
+    (write-region (point-min) (point-max) dedicated-session-journal)
+    )
+  (kill-current-buffer)
+  (cl-incf dedicated-session-log-number-this-session)
+  ;;when the log is summary, call releasing prompt.
+  (when summary?
+    (dedicated-session-releasing-prompt))
+  )
+
+
+(defun dedicated-session-releasing ()
+  "end the last session, enter summary log buffer, which, after finalized, test if it's a summary. After tested summary positive, it will proceed to releasing prompt buffer, which the releasing process go on."
+  (interactive)
+  ;(setq dedicated-session-in 'releasing)
+  (dedicated-session-bar-rest-toggle t)
+  (dedicated-session-log t)
+ ; (dedicated-session-releasing-prompt)
+  )
+
+(defun dedicated-session-releasing-prompt ()
+  "open the releasing buffer"
+  (dedicated-session-rest-prompt dedicated-session-default-releasing-prompt-fmt)
   )
 
 
